f8c8a23367fc3f5f56c6000a697c5ab871c30738
yuhonglei <yuhonglei86@gmail.com>
2023-12-16 16:33:02 +0800

    [新增]反向提示词

--- scripts/prompt-templates.py
+++ scripts/prompt-templates.py
@@ -20,6 +20,10 @@ def send_text_to_prompt(new_text, old_text):
     # return old_text + " " + new_text
 
 
+def send_text_to_negative_prompt(new_text, old_text):
+    return new_text
+
+
 class TemplateScript(scripts.Script):
     """自定义脚本类，用于提供 Gradio 界面功能"""
 
@@ -41,11 +45,18 @@ class TemplateScript(scripts.Script):
                 return data
         return []
 
-    def update_textbox(self, selected_name):
+    def update_prompt(self, selected_name):
         """根据下拉菜单的选择更新文本框"""
         for item in self.template_data:
             if item["name"] == selected_name:
                 return item["prompt"]
+        return ""
+
+    def update_negative_prompt(self, selected_name):
+        """根据下拉菜单的选择更新文本框"""
+        for item in self.template_data:
+            if item["name"] == selected_name:
+                return item["negative_prompt"]
         return ""
 
     def show(self, is_img2img):
@@ -56,25 +67,32 @@ class TemplateScript(scripts.Script):
         """构建 UI 组件"""
         with gr.Accordion('提示词模板', open=False):
             with gr.Column():
+                dropdown_to_text = gr.Dropdown(
+                    [item["name"] for item in self.template_data],
+                    label="选择模板",
+                    value=self.template_data[0]["name"] if self.template_data else "无选项"
+                )
                 with gr.Row():
-                    text_to_be_sent = gr.Textbox(label="模板描述")
-                    dropdown_to_text = gr.Dropdown(
-                        [item["name"] for item in self.template_data],
-                        label="选择模板",
-                        value=self.template_data[0]["name"] if self.template_data else "无选项"
-                    )
+                    prompt_sent = gr.Textbox(label="正向提示词")
+                    negative_prompt_send = gr.Textbox(label="反向提示词")
+
                 send_text_button = gr.Button(value='发送到提示词框', variant='primary')
-                dropdown_to_text.change(fn=self.update_textbox, inputs=[dropdown_to_text], outputs=[text_to_be_sent])
+                dropdown_to_text.change(fn=self.update_prompt, inputs=[dropdown_to_text], outputs=[prompt_sent])
+                dropdown_to_text.change(fn=self.update_negative_prompt, inputs=[dropdown_to_text],
+                                        outputs=[negative_prompt_send])
 
         # 处理文本框和按钮交互
         with contextlib.suppress(AttributeError):
             if is_img2img:
-                send_text_button.click(fn=send_text_to_prompt, inputs=[text_to_be_sent, self.boxxIMG],
+                send_text_button.click(fn=send_text_to_prompt, inputs=[prompt_sent, self.boxxIMG],
                                        outputs=[self.boxxIMG])
+                send_text_button.click(fn=send_text_to_negative_prompt, inputs=[negative_prompt_send, self.neg_prompt_boxIMG],
+                                       outputs=[self.neg_prompt_boxIMG])
             else:
-                send_text_button.click(fn=send_text_to_prompt, inputs=[text_to_be_sent, self.boxx], outputs=[self.boxx])
+                send_text_button.click(fn=send_text_to_prompt, inputs=[prompt_sent, self.boxx], outputs=[self.boxx])
+                send_text_button.click(fn=send_text_to_negative_prompt, inputs=[negative_prompt_send, self.neg_prompt_boxTXT], outputs=[self.neg_prompt_boxTXT])
 
-        return [text_to_be_sent, dropdown_to_text, send_text_button]
+        return [prompt_sent, dropdown_to_text, send_text_button]
 
     def after_component(self, component, **kwargs):
         """处理组件事件后的回调函数"""
@@ -82,3 +100,7 @@ class TemplateScript(scripts.Script):
             self.boxx = component
         if kwargs.get("elem_id") == "img2img_prompt":
             self.boxxIMG = component
+        if kwargs.get("elem_id") == "txt2img_neg_prompt":
+            self.neg_prompt_boxTXT = component
+        if kwargs.get("elem_id") == "img2img_neg_prompt":
+            self.neg_prompt_boxIMG = component
