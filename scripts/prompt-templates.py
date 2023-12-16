6c71d9925669c97c098d2eb32796ff714030d3ff
yuhonglei <yuhonglei86@gmail.com>
2023-12-16 14:29:14 +0800

    [完善]UI

--- scripts/prompt-templates.py
+++ scripts/prompt-templates.py
@@ -1,64 +1,46 @@
 import contextlib
 
 import gradio as gr
-from modules import scripts
-from modules import script_callbacks
+import modules.scripts as scripts
+from modules.processing import process_images
 
 def send_text_to_prompt(new_text, old_text):
     if old_text == "":  # if text on the textbox text2img or img2img is empty, return new text
         return new_text
     return old_text + " " + new_text  # else join them together and send it to the textbox
-
-class ExampleScript(scripts.Script):
-    def __init__(self) -> None:
-        super().__init__()
-
+class ExtensionTemplateScript(scripts.Script):
+    # Extension title in menu UI
     def title(self):
-        return "Example"
+        return "Prompt Template"
 
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
+    # Setup menu ui detail
     def ui(self, is_img2img):
-        with gr.Group():
-            with gr.Accordion("Send centrum", open=False):
-                send_text_button = gr.Button(value='send text', variant='primary')
-                text_to_be_sent = gr.Textbox(label="drop text")
+        with gr.Accordion('提示词模板', open=False):
+            with gr.Column():
+                with gr.Row():
+                    text_to_be_sent = gr.Textbox(label="模板描述")
+                    dropdown_to_be_text = gr.Dropdown(
+                        ["base", "cat", "dog", "bird"], label="选择模板", value="base"
+                    )
+                send_text_button = gr.Button(value='发送到提示词框', variant='primary')
 
         with contextlib.suppress(AttributeError):  # Ignore the error if the attribute is not present
             if is_img2img:
-                # Bind the click event of the button to the send_text_to_prompt function
-                # Inputs: text_to_be_sent (textbox), self.boxxIMG (textbox)
-                # Outputs: self.boxxIMG (textbox)
-                send_text_button.click(fn=send_text_to_prompt, inputs=[text_to_be_sent, self.boxxIMG], outputs=[self.boxxIMG])
+                send_text_button.click(fn=send_text_to_prompt, inputs=[text_to_be_sent, self.boxxIMG],
+                                       outputs=[self.boxxIMG])
             else:
-                # Bind the click event of the button to the send_text_to_prompt function
-                # Inputs: text_to_be_sent (textbox), self.boxx (textbox)
-                # Outputs: self.boxx (textbox)
                 send_text_button.click(fn=send_text_to_prompt, inputs=[text_to_be_sent, self.boxx], outputs=[self.boxx])
 
-        return [text_to_be_sent, send_text_button]
+        return [text_to_be_sent, dropdown_to_be_text, send_text_button]
 
     def after_component(self, component, **kwargs):
-        #https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/7456#issuecomment-1414465888 helpfull link
-        # Find the text2img textbox component
-        if kwargs.get("elem_id") == "txt2img_prompt": #postive prompt textbox
+
+        if kwargs.get("elem_id") == "txt2img_prompt":
             self.boxx = component
-        # Find the img2img textbox component
-        if kwargs.get("elem_id") == "img2img_prompt":  #postive prompt textbox
+        if kwargs.get("elem_id") == "img2img_prompt":
             self.boxxIMG = component
-
-        #this code below  works aswell, you can send negative prompt text box,provided you change the code a little
-        #switch  self.boxx with  self.neg_prompt_boxTXT  and self.boxxIMG with self.neg_prompt_boxIMG
-
-        #if kwargs.get("elem_id") == "txt2img_neg_prompt":
-            #self.neg_prompt_boxTXT = component
-        #if kwargs.get("elem_id") == "img2img_neg_prompt":
-            #self.neg_prompt_boxIMG = component
-
-
-
-
-
 
 
